40d55062bb475e383020fda3874a4ba57414c6e4
jordan <abc@example.com>
2020-02-03 12:27:08 -0500

    c document with blank methods to be filled in

--- 306PRE_project.c
+++ 306PRE_project.c
@@ -1,3 +1,37 @@
-printf("test");
-printf("lol");
-printf("testing");
+# include <stdio.h>
+
+int main(int argc, char*argv[]){
+		///TODO: READ COMMAND ARGUEMENTS AND CALL BELOW FUNCTIONS 
+}
+
+void readcvs(){
+		///TODO: READ AND PARSE CSV FILE
+}
+
+void f(){
+		///TODO: Display the number of fields in the first record of file
+}
+
+void r(){
+		///TODO: Display the number of data records in file
+}
+
+void h(){
+		///TODO: Treat the first record of file as a header record rather than a data record
+}
+
+void max(){
+		///TODO: Display the maximum value in the indicated field of the data records
+}
+
+void min(){
+		///TODO: Display the minimum value in the indicated field of the data records
+}
+
+void mean(){
+		///TODO: Display the arithmetic mean (average) value in the indicated field of the data records
+}
+
+void records(){
+		///TODO: Display the records from file containing value in the the indicated field
+}

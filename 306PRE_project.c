06741f699544fdbff500eec6d0be1463c0695e00
jordan <abc@example.com>
2020-02-03 17:30:16 -0500

    Added parameters to master where needed

--- 306PRE_project.c
+++ 306PRE_project.c
@@ -4,7 +4,7 @@ int main(int argc, char*argv[]){
 		///TODO: READ COMMAND ARGUEMENTS AND CALL BELOW FUNCTIONS 
 }
 
-void readcvs(){
+void readcvs(char field[]){
 		///TODO: READ AND PARSE CSV FILE
 }
 
@@ -20,18 +20,18 @@ void h(){
 		///TODO: Treat the first record of file as a header record rather than a data record
 }
 
-void max(){
+void max(char field[]){
 		///TODO: Display the maximum value in the indicated field of the data records
 }
 
-void min(){
+void min(char field[]){
 		///TODO: Display the minimum value in the indicated field of the data records
 }
 
-void mean(){
+void mean(char field[]){
 		///TODO: Display the arithmetic mean (average) value in the indicated field of the data records
 }
 
-void records(){
+void records(char field[]){
 		///TODO: Display the records from file containing value in the the indicated field
 }
